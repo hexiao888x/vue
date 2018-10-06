e3ad78011fe19bf056a0ca6a062cb64ff65a3413
hexiao888x <hexiao888x@163.com>
2018-10-06 11:25:06 +0800

    路由设置

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
     <div>
         <mt-header id="topheader" fixed title="固定在顶部"></mt-header>
         <h1>123</h1>
-
+<mt-switch v-model="value">开关</mt-switch>
         <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
 				<span class="mui-tab-label">消息</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-gear"></span>
 				<span class="mui-tab-label">设置</span>
-			</a>
+			</router-link>
 		</nav>
         
     </div>
@@ -28,9 +28,6 @@
  
 </script>
 <style>
-#topheader{
-    background-color: cornflowerblue;
-    text-align: center;
-}
+
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 import { Header } from 'mint-ui'
-
+import 'mint-ui/lib/header/style.css'
 Vue.component(Header.name, Header)
+import { Switch } from 'mint-ui';
+import 'mint-ui/lib/switch/style.css'
+Vue.component(Switch.name, Switch);
+import router from './router.js'
 
 import app from './App.vue'
 var vm = new Vue({
     el:'#app',
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 })
